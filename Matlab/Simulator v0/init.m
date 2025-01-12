d871699bf9db334bbd347ef31c6adc26cae82aba
Anthony Andreassen <69370185+spaceeDolphin@users.noreply.github.com>
2025-01-12 13:10:22 +0100

    simulink sim

--- Matlab/Simulator v0/init.m
+++ Matlab/Simulator v0/init.m
@@ -1,3 +1,11 @@
 %Mathematical model of tank with mass flow function based on inflow, outflow
 %MPC optimization: minimize run costs
 %Constraints: min/max levels, pump capacity
+
+h = 7;
+A = 380;
+V = h*A;
+qV_max = 120/3600; % m3/s
+qOut_median = 30/3600;
+
+list24 = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]*3600;
